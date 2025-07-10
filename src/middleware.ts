43d31fc2a99f655f1f0b98992e1a791c219e3f9e
Joao Victor Assis <joaovictorassis03@gmail.com>
2025-07-10 04:04:03 -0300

    feature: Implementação das validações de sessões via JWT

---
+++ src/middleware.ts
@@ -0,0 +1,38 @@
+import { isSessionValid } from "@/app/lib/session";
+import {NextRequest, NextResponse} from "next/server";
+
+
+
+//regex retirada diretamente da documentação do NextJS
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+};
+
+const publicRoutes = [
+    '/',
+    '/login',
+    '/create'
+]
+
+export async function middleware(req: NextRequest){
+
+    const pathname = req.nextUrl.pathname;
+
+    //verificar se a requisicao possui credenciais validas para criar uma session
+    const session = await isSessionValid();
+
+    if(publicRoutes.includes(pathname) && session)
+    {
+        return NextResponse.redirect(new URL('/main/list', req.nextUrl));
+    }
+    
+    if(!session && !publicRoutes.includes(pathname)){
+        return NextResponse.redirect(new URL('/login', req.nextUrl));
+    }
+
+    
+    return NextResponse.next();
+      
+
+}
